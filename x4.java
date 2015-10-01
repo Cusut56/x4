0b1a5c5bae0ef137b3bfcda47a447571ea4cfd3d
Cusut56 <tarashere2005@yahoo.com>
2015-09-30 21:06:37 -0400

    Update x4.java

--- x4.java
+++ x4.java
@@ -1,4 +1,6 @@
-float button1x=250,button1y=250,button1w=80,button1h=40;
+int r,g,b;
+float button1X=100, button1Y=100, button1W=80, button1H=40;
+int counter=0;
 
 
 void setup() {
@@ -7,11 +9,45 @@ reset();
 
 }
 void reset(){
-
+r=100;
+g=200;
+b=250;
 }
+
 void draw(){
-background(255,255,0);
-showButton(button1x,button1y,button1w,button1h);
-fill(150,10,10);
-text("click me!",100+80/4,100+40*2/3);
+  background(r,g,b);
+rect(button1X,button1Y,button1W,button1H);
+  fill(150,10,10);
+    text("click me!",100+80/4,100+40*2/3);
+    fill(200,200,10);
+}
+void showbutton(float x, float y, float w,float h){
+  fill(255,255,0);
+  rect(x,y,w,h);
+}
+void keypressed(){
+if (key=='q') exit();
+if (key=='r') reset();
+}
+void mousePressed(){
+  if( hit (mouseX,mouseY,100,100,50,50) ){
+    counter=counter+1;
+    if (counter %2>0){
+      r=255;
+      g=50;
+      b=0;
+    }else{
+      reset();
+    }
+  }
+}
+boolean hit(float x1,float y1,float x2,float y2,float w,float h){
+  boolean result;
+  
+  if (abs(x1-x2)<w && abs(y1-y2)<h){
+    result = true;
+  }else{
+    result=false;
+  }
+  return result;
 }
